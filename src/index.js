ff850ce5f828d329535c85c6dbcd9bc3ad45332f
sahaj <sahajjain13@gmail.com>
2022-04-02 02:20:12 +0530

    Setup React router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import RouteSwitch from './RouteSwitch';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouteSwitch />
   </React.StrictMode>,
   document.getElementById('root'),
 );
