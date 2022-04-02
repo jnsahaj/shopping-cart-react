dace4509f63e7bacb98c52f46be2ce39f015fc65
sahaj <sahajjain13@gmail.com>
2022-04-02 06:36:24 +0530

    Rename RouteSwitch to App and add change handlers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Home from './components/pages/Home/Home';
+import Shop from './components/pages/Shop/Shop';
+import Cart from './components/Cart/Cart';
 
 function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  const handleAddToCart = (item) => {
+    setCartItems([...cartItems, { ...item, quantity: 1 }]);
+  };
+
+  const handleQuantityChange = (itemId, quantity) => {
+    setCartItems(cartItems.map((item) => (item.id === itemId
+      ? { ...item, quantity }
+      : item
+    )));
+  };
+
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/shop"
+          element={<Shop onAddToCart={handleAddToCart} />}
+        />
+        <Route
+          path="/cart"
+          element={(
+            <Cart
+              items={cartItems}
+              onQuantityChange={handleQuantityChange}
+            />
+          )}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import RouteSwitch from './RouteSwitch';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <RouteSwitch />
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
