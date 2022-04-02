5504f58ce56b81dfd36eed1582cb19af4b4028e0
sahaj <sahajjain13@gmail.com>
2022-04-02 19:07:51 +0530

    Lift state of shopItems and fetch data from App component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/pages/Home/Home';
 import Shop from './components/pages/Shop/Shop';
 import Cart from './components/Cart/Cart';
+import ShopData from './data/ShopData.json';
+import './App.css';
 
 function App() {
+  const [shopItems, setShopItems] = useState([]);
   const [cartItems, setCartItems] = useState([]);
 
-  const handleAddToCart = (item) => {
-    setCartItems([...cartItems, { ...item, quantity: 1 }]);
+  const handleAddToCart = (itemId) => {
+    if (cartItems.some((item) => item.id === itemId)) return;
+    const shopItem = shopItems.find((item) => item.id === itemId);
+    setCartItems([...cartItems, { ...shopItem, quantity: 1 }]);
   };
 
   const handleQuantityChange = (itemId, quantity) => {
@@ -23,6 +28,14 @@ function App() {
     setCartItems(cartItems.filter((item) => item.id !== itemId));
   };
 
+  const fetchItems = async () => {
+    setShopItems(ShopData.map((item) => ({ ...item, price: item.price.slice(1, item.price.indexOf('-')) })));
+  };
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
   return (
     <BrowserRouter>
       <Header />
@@ -33,7 +46,12 @@ function App() {
         />
         <Route
           path="/shop"
-          element={<Shop onAddToCart={handleAddToCart} />}
+          element={(
+            <Shop
+              items={shopItems}
+              onAddToCart={handleAddToCart}
+            />
+          )}
         />
         <Route
           path="/cart"
