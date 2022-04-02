2a24a9033515928514e384e7069759a80ca1e941
sahaj <sahajjain13@gmail.com>
2022-04-02 05:52:15 +0530

    Add ItemCard component with AddToCart handler

---
+++ src/components/ItemCard/ItemCard.js
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import './ItemCard.css';
+
+function ItemCard({
+  id, imgSrc, name, price, onAddToCart,
+}) {
+  const handleAddToCart = () => {
+    onAddToCart(id);
+  };
+
+  return (
+    <div className="item">
+      <div className="img-wrapper">
+        <img src={imgSrc} alt={name} />
+      </div>
+      <div className="content-wrapper">
+        <div className="name">{name}</div>
+        <div className="price">{`$${price}`}</div>
+      </div>
+      <button type="button" className="add-to-cart" onClick={handleAddToCart}>
+        Add to Cart
+      </button>
+    </div>
+  );
+}
+
+// ItemCard.PropTypes = {
+//   imgSrc: PropTypes.string,
+// };
+
+export default ItemCard;
