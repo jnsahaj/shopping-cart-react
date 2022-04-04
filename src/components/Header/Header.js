50601fa3ba1d6915b1a5083001bb9d56aeb3f494
sahaj <sahajjain13@gmail.com>
2022-04-05 02:59:52 +0530

    Import and show logo in Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiShoppingBag, FiShoppingCart } from 'react-icons/fi';
 import PropTypes from 'prop-types';
+import logo from '../../assets/logo.png';
 import './Header.css';
 
 function Header({ cartItemsCount }) {
@@ -9,8 +10,7 @@ function Header({ cartItemsCount }) {
     <div className="header">
       <Link to="/">
         <div className="logo-wrapper">
-          {/* <img src="/" alt="Logo" /> */}
-          Home
+          <img src={logo} alt="Logo" />
         </div>
       </Link>
       <nav className="navbar">
@@ -18,14 +18,14 @@ function Header({ cartItemsCount }) {
           <Link to="/shop">
             <li>
               <FiShoppingBag />
-              <div>Shop</div>
+              <div className="link-names">Shop</div>
             </li>
           </Link>
           <Link to="/cart">
             <li>
               {cartItemsCount > 0 && <div className="cart-items-badge">{cartItemsCount}</div>}
               <FiShoppingCart />
-              <div>Cart</div>
+              <div className="link-names">Cart</div>
             </li>
           </Link>
         </ul>
