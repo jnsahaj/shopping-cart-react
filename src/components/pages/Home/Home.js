205fe6ffe39b67be0bc1ebd32788e24f73d07d29
sahaj <sahajjain13@gmail.com>
2022-04-05 03:02:24 +0530

    Add logo and background to Home component

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../../assets/logo.png';
+import './Home.css';
 
 function Home() {
   return (
     <div className="home page">
-      <Link to="/shop">
-        <button type="button">Shop</button>
-      </Link>
+      <div className="logo-wrapper">
+        <img src={logo} alt="Logo" />
+        <div className="shop-at">
+          Vertigo
+        </div>
+      </div>
+      <div className="shop">
+        <Link to="/shop">
+          <button type="button">Shop</button>
+        </Link>
+      </div>
     </div>
   );
 }
