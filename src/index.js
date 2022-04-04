264a7bfb82602422ff4cc856114ba078696f8c30
sahaj <sahajjain13@gmail.com>
2022-04-05 02:58:58 +0530

    Updated render to React 18

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </StrictMode>,
 );
